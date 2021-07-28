561451580efa7e7208554891437ee3b69edc9305
unknown <jala.santoshreddy@gmail.com>
2021-07-28 00:19:59 -0500

    Added Main content,Routes and Links

---
+++ src/screens/mainContent.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import AddClient from "./addClient";
+import AddProject from "./addProject";
+
+const MainContent = () => {
+  return (
+    <div>
+      <Switch>
+        <Route path="/addClient">
+          <AddClient />
+        </Route>
+        <Route path="/addProject">
+          <AddProject />
+        </Route>
+        <Route path="/">
+          <h1>In Development</h1>
+        </Route>
+      </Switch>
+    </div>
+  );
+};
+
+export default MainContent;
--- src/screens/mainScreen.tsx
+++ src/screens/mainScreen.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { Layout, Menu, Breadcrumb } from "antd";
 import {
   DesktopOutlined,
@@ -7,8 +6,13 @@ import {
   TeamOutlined,
   UserOutlined,
 } from "@ant-design/icons";
-import Sider from "antd/lib/layout/Sider";
-import { Content, Footer, Header } from "antd/lib/layout/layout";
+import { useState } from "react";
+import MainContent from "./mainContent";
+import "./mainScreen.less";
+import { Link } from "react-router-dom";
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
 const MainScreen = () => {
   const [collapsed, setCollapsed] = useState(false);
@@ -18,35 +22,31 @@ const MainScreen = () => {
   };
 
   return (
-    <Layout>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider
+        id="mainSideBar"
+        collapsible
+        collapsed={collapsed}
+        onCollapse={onCollapse}
+      >
         <div className="logo" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-          <Menu.Item key="1" icon={<UserOutlined />}>
-            nav 1
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            <Link to="/addClient">Add Client</Link>
+          </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            <Link to="/addProject">Add Project</Link>
           </Menu.Item>
         </Menu>
       </Sider>
       <Layout className="site-layout">
-        <Header className="site-layout-background" style={{ padding: 0 }}>
-          {/* {React.createElement(
-            this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
-            {
-              className: "trigger",
-              onClick: this.toggle,
-            }
-          )} */}
-        </Header>
-        <Content
-          className="site-layout-background"
-          style={{
-            margin: "24px 16px",
-            padding: 24,
-            minHeight: 280,
-          }}
-        >
-          Content
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: "0 16px" }}>
+          <MainContent />
         </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
       </Layout>
     </Layout>
   );
